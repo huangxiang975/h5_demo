433f90d731cd70ae7817a0074824bd3c4124ca09
sean <huangxiang975@163.com>
2019-05-17 16:49:35 +0800

    增加时间处理器

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,17 @@ import App from "./App.vue";
 import router from "./router/index";
 import store from "./store/index";
 import MintUI from "mint-ui";
+import moment from "moment"; // 解决对时间的处理
 import "reset-css";
 Vue.use(MintUI);
-import "mint-ui/lib/style.css"; // mint-ui样式
 
-import "./utils/rem";
+import "mint-ui/lib/style.css"; // mint-ui样式
+import "./utils/rem"; // rem自动计算代码  设计比例 vw > 750px ? 1rem = 75px : 1rem = vw/750*100 px;
+import "./utils/permission"; // 路由拦截器
 
 Vue.config.productionTip = false;
 
-import "./utils/permission"; // 路由拦截器
+Vue.prototype.$moment = moment;
 
 // axios.defaults.baseURL = "http://101.228.87.56:2091";
 
